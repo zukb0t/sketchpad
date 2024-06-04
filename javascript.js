5ef7f4739a6cde264f1bcdc802a8f0cd54d2f5a9
Duong Tang <dttang1994@gmail.com>
2024-06-03 17:20:33 -0700

    wrote down some stuff for sketchpad.

--- javascript.js
+++ javascript.js
@@ -8,3 +8,9 @@ const eraser = document.querySelector(".eraser");
 const clear = document.querySelector(".clear");
 const marker = document.querySelector(".marker");
 const random = document.querySelector(".random");
+
+sketch_area.classList.add("sketch_area");
+sketch_area.style.width = `${area}px`;
+sketch_area.style.height = `${area}px`;
+sketch_area.addEventListener("mousedown", () => {mouseDown = true;});
+sketch_area.addEventListener("mouseup", () => {mouseDown = false;});
