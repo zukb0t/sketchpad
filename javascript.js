7b9a347c5040ae6963f032fdab5a4f9e32fde8d8
Duong Tang <dttang1994@gmail.com>
2024-06-04 01:09:50 -0700

    added buttons and got them working as intended.

--- javascript.js
+++ javascript.js
@@ -5,7 +5,6 @@ const container = document.querySelector(".container");
 const sketch_area = document.createElement("div");
 const resize = document.querySelector(".resize");
 const eraser = document.querySelector(".eraser");
-const clear = document.querySelector(".clear");
 const marker = document.querySelector(".marker");
 const random = document.querySelector(".random");
 
@@ -15,11 +14,20 @@ sketch_area.style.height = `${area}px`;
 sketch_area.addEventListener("mousedown", () => {mouseDown = true;});
 sketch_area.addEventListener("mouseup", () => {mouseDown = false;});
 
+const getRandom = (number) => Math.floor(Math.random()*(number+1));
+
+function resizeGrid(event){
+    console.log(sketch_area);
+    sketch_area.querySelectorAll(".square").forEach((event)=>event.parentNode.removeChild(event));
+    let n = prompt("enter the number of squares per side.");
+    drawBoard(n);
+}
+
 function drawBoard(size){
     const grid_size = parseInt(size);
     for(let i = 0; i < size*size; i++){
         const square = document.createElement("div");
-        square.classList.add(".square");
+        square.classList.add("square");
         square.style.width = `${((area/size) - 2)}px`;
         square.style.height =`${((area/size) - 2)}px`;
         square.addEventListener("mouseover", changeColor);
@@ -27,5 +35,23 @@ function drawBoard(size){
     }
 }
 
+function changeColor(event){
+    if(event.type==="mouseover" && !mouseDown){return;}
+    if(choice==="default_color"){
+        event.target.style.backgroundColor = "black";
+    }
+    else if(choice==="random"){
+        const random_color = `rgb(${getRandom(255)}, ${getRandom(255)}, ${getRandom(255)})`;
+        event.target.style.backgroundColor = random_color;
+    }
+    else if(choice==="eraser"){
+        event.target.style.backgroundColor = "white";    
+    }
+}
+
+marker.addEventListener("click", () => choice = "default_color");
+random.addEventListener("click", () => choice = "random");
+eraser.addEventListener("click", () => choice = "eraser");
+resize.addEventListener("click", resizeGrid);
 container.appendChild(sketch_area);
-drawBoard(16);
+drawBoard("16");
